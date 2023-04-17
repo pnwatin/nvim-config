df86e15cbf8c282b6c09b02d7191e5d0b73fb1a6
Nicolas <pnwa@icloud.com>
2023-04-17 17:36:42 +0200

    chore: add moonfly colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", opts = {
-    flavour = "mocha",
-  } },
+  { "catppuccin/nvim", name = "catppuccin" },
+  { "bluz71/vim-moonfly-colors", name = "moonfly" },
+  { "bluz71/vim-nightfly-colors", name = "nightfly" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "moonfly",
     },
   },
 }
