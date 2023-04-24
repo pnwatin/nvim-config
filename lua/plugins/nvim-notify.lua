0f5e7e62f4cdbd93527182e23ddfa235e42287e2
Nicolas <pnwa@icloud.com>
2023-04-24 17:28:03 +0200

    chore: disable notify

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -1,6 +1,9 @@
-return { {
-  "rcarriga/nvim-notify",
-  opts = {
-    background_colour = "#000000",
+return {
+  {
+    "rcarriga/nvim-notify",
+    -- opts = {
+    --   background_colour = "#000000",
+    -- },
+    enabled = false,
   },
-} }
+}
