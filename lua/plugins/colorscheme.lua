f55b07dd838b6099d193d9428f13ac23d7b5e2ac
Nicolas <pnwa@icloud.com>
2023-06-29 13:49:38 +0200

    chore: colorscheme update and disabled auto plugin update check

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,18 @@
 return {
   { "bluz71/vim-nightfly-colors", name = "nightfly" },
+  { "bluz71/vim-moonfly-colors", name = "moonfly" },
+  {
+    "Mofiqul/vscode.nvim",
+    lazy = true,
+    name = "vscode",
+    otps = {
+      transparent = true,
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nightfly",
+      colorscheme = "moonfly",
     },
   },
 }
