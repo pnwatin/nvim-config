8dabb3d453d2bba9be82ad20366513912c7849fd
Nicolas <pnwa@icloud.com>
2023-09-02 02:43:42 +0200

    chore: add nvim-notify settings but keeps it disabled

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,8 +1,16 @@
-return { {
-  "folke/noice.nvim",
-  opts = {
-    presets = {
-      lsp_doc_border = true,
+return {
+  {
+    "folke/noice.nvim",
+    opts = {
+      presets = {
+        lsp_doc_border = true,
+      },
     },
   },
-} }
+  -- {
+  --   "rcarriga/nvim-notify",
+  --   opts = {
+  --     background_colour = "#000000",
+  --   },
+  -- },
+}
