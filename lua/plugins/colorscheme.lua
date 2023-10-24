d5d203f442ca98e8c41ac4f6b1dcf83b7c5e7f86
Nicolas <pnwa@icloud.com>
2023-10-24 11:10:50 +0200

    chore: make catpuccin more contrasted

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,6 +4,13 @@ return {
     name = "catppuccin",
     opts = {
       show_end_of_buffer = true,
+      color_overrides = {
+        mocha = {
+          base = "#11111b",
+          mantle = "#11111b",
+          crust = "#11111b",
+        },
+      },
     },
   },
   {
