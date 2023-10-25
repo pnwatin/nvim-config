8cd12082a9daa1e67915658ff1a932c2b3fc391a
Nicolas <pnwa@icloud.com>
2023-10-25 17:13:18 +0200

    chore: for tsserver non-relative imports

--- lua/plugins/ts-tools.lua
+++ lua/plugins/ts-tools.lua
@@ -4,6 +4,7 @@ return {
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     opts = {
       settings = {
+        tsserver_file_preferences = { importModuleSpecifierPreference = "non-relative" },
         expose_as_code_action = {
           "fix_all",
           "add_missing_imports",
