a3e09f83a59411bdc00f01bc35534b6d31060b32
Nicolas <pnwa@icloud.com>
2023-10-26 18:27:56 +0200

    chore: tweaks options (fold and scroll context)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -24,9 +24,11 @@ opt.relativenumber = true
 opt.numberwidth = 3
 
 opt.softtabstop = 2
-opt.scrolloff = 8
+opt.scrolloff = 15
+opt.sidescrolloff = 15
 
 opt.iskeyword:append("-")
 
-opt.foldmethod = "indent"
+-- opt.foldmethod = "indent"
 opt.foldenable = false
+opt.foldnestmax = 1
