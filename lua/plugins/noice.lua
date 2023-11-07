ddd593e7fa3c566420d8a3066f6104ca08d542ad
Nicolas <pnwa@icloud.com>
2023-11-07 23:49:03 +0100

    chore: make noice less bloated

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -4,6 +4,16 @@ return {
     opts = {
       presets = {
         lsp_doc_border = true,
+        command_palette = false,
+      },
+      cmdline = {
+        view = "cmdline",
+        format = {
+          cmdline = { pattern = "^:", icon = ":", lang = "vim" },
+          search_down = { kind = "search", pattern = "^/", icon = "/", lang = "regex" },
+          search_up = { kind = "search", pattern = "^%?", icon = "/", lang = "regex" },
+          help = { pattern = "^:%s*he?l?p?%s+", icon = "?" },
+        },
       },
     },
   },
