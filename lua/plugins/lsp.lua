06f2d05d3e3ad2641d6b7bc3497f16ebfbc05fad
Nicolas <pnwa@icloud.com>
2023-11-09 16:07:00 +0100

    chore: add prismals and remap ca on rust_analyzer

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,6 +3,12 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        prismals = {},
+        rust_analyzer = {
+          keys = {
+            { "<leader>ca", "<cmd>RustCodeAction<cr>", desc = "Code Action (Rust)" },
+          },
+        },
         emmet_language_server = {},
         html = {
           filetypes = {
