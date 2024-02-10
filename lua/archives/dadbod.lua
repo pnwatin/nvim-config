da79691177f9841fd302bb66c4865369e1d982c6
Nicolas <pnwa@icloud.com>
2024-02-10 22:42:01 +0100

    chore: move dadbod config to archives

---
+++ lua/archives/dadbod.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      {
+        "kristijanhusak/vim-dadbod-completion",
+        ft = { "sql", "mysql", "plsql" },
+        lazy = true,
+      },
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    keys = {
+      {
+        "<leader>db",
+        "<cmd>DBUIToggle<cr>",
+        desc = "toggle dadbod ui",
+      },
+    },
+    init = function()
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "vim-dadbod-completion" } }))
+    end,
+  },
+}
