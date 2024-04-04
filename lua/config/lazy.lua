c63ab4d8af29a5fe0c1c9bb9a0d7dbed9f4f9b61
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2024-04-04 22:03:47 +0200

    chore: undo disabling netrw plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -54,7 +54,7 @@ require("lazy").setup({
         "gzip",
         "matchit",
         "matchparen",
-        "netrwPlugin",
+        -- "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
