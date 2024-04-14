c41c27f9c2d8444986948be12d5e4910cfd4c85a
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2024-04-14 19:29:07 +0200

    chore: remove some colorscheme overrides

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,8 +7,7 @@ return {
       color_overrides = {
         mocha = {
           base = "#11111b",
-          mantle = "#11111b",
-          crust = "#11111b",
+          -- mantle = "#11111b",
         },
       },
       integrations = {
