827b926f1f5dd771de7c812618462180946b7d26
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2024-04-28 00:23:11 +0200

    deps: remove ts-tools and go back to tsserver

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,8 +2,50 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      -- inlay_hints = { enabled = true },
       servers = {
+        tsserver = {
+          keys = {
+            {
+              "<leader>ru",
+              function()
+                vim.lsp.buf.code_action({
+                  apply = true,
+                  context = {
+                    only = { "source.removeUnused.ts" },
+                    diagnostics = {},
+                  },
+                })
+              end,
+              desc = "Remove Unused",
+            },
+            {
+              "<leader>ri",
+              function()
+                vim.lsp.buf.code_action({
+                  apply = true,
+                  context = {
+                    only = { "source.removeUnusedImports.ts" },
+                    diagnostics = {},
+                  },
+                })
+              end,
+              desc = "Remove Unused Imports",
+            },
+            {
+              "<leader>ai",
+              function()
+                vim.lsp.buf.code_action({
+                  apply = true,
+                  context = {
+                    only = { "source.addMissingImports.ts" },
+                    diagnostics = {},
+                  },
+                })
+              end,
+              desc = "Add Missing Imports",
+            },
+          },
+        },
         prismals = {},
         emmet_language_server = {},
         html = {
