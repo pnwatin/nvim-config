2556bc72450911b0bfd3a8c5f6e93ce81dfe94dd
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2024-05-14 15:30:21 +0200

    chore: add a temporary file for fixes

---
+++ lua/plugins/temporary.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      pickers = {
+        lsp_definitions = {
+          jump_type = "never",
+        },
+      },
+    },
+  },
+}
