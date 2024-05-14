4bca3614d0ecc6a9ae4d3124c78ad1c4b239e2b8
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2024-05-14 19:29:32 +0200

    fix: bump telescope

--- lua/plugins/temporary.lua
+++ lua/plugins/temporary.lua
@@ -1,12 +1,1 @@
-return {
-  {
-    "nvim-telescope/telescope.nvim",
-    opts = {
-      pickers = {
-        lsp_definitions = {
-          jump_type = "never",
-        },
-      },
-    },
-  },
-}
+return {}
