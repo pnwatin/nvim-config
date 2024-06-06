d0656aec8477a0f8ec52866227ab7e61a32f7084
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2024-06-06 17:45:49 +0200

    extra: add git lang

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.git" },
 
     -- lsp
 
