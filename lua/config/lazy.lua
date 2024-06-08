cf8c56661b05fdd023e2a1a4c2c9afd57b05d630
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2024-06-08 20:18:02 +0200

    deps: bump plugins and add sql extras

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,14 +13,17 @@ require("lazy").setup({
     -- import any extras modules here
     -- languages
     { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+
+    { import = "lazyvim.plugins.extras.lang.git" },
+    { import = "lazyvim.plugins.extras.lang.sql" },
+
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.docker" },
-    { import = "lazyvim.plugins.extras.lang.git" },
 
     -- lsp
 
