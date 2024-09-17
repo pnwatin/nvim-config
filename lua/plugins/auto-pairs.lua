3525c81d744c19a5fff9e050578b701fb256ea55
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2024-09-17 21:22:37 +0200

    chore: disable "'" autopair in rust files

--- lua/plugins/auto-pairs.lua
+++ lua/plugins/auto-pairs.lua
@@ -5,6 +5,13 @@ return {
     branch = "v0.6",
     opts = {
       config_internal_pairs = {
+        {
+          "'",
+          "'",
+          suround = true,
+          nft = { "rust" },
+          multiline = true,
+        },
         { "{", "}", suround = true, multiline = true },
         { "[", "]", suround = true, multiline = true },
         { "(", ")", suround = true, multiline = true },
