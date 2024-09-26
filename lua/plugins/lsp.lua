7508d663bf90eb808fd3b658efc6aeec78124250
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2024-09-26 15:19:54 +0200

    chore: disable diagnostics on .env* files

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,6 +9,16 @@ return {
         enabled = false,
       },
       servers = {
+        bashls = {
+          handlers = {
+            ["textDocument/publishDiagnostics"] = function(err, res, ...)
+              local file_name = vim.fn.fnamemodify(vim.uri_to_fname(res.uri), ":t")
+              if string.match(file_name, "^%.env") == nil then
+                return vim.lsp.diagnostic.on_publish_diagnostics(err, res, ...)
+              end
+            end,
+          },
+        },
         vtsls = {
           keys = {
             {
