86f1bb2721391c75ab81b40781c71a00f18932fd
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2024-10-02 17:24:44 +0200

    chore: bumps deps

--- lua/plugins/auto-pairs.lua
+++ lua/plugins/auto-pairs.lua
@@ -5,13 +5,6 @@ return {
     branch = "v0.6",
     opts = {
       config_internal_pairs = {
-        {
-          "'",
-          "'",
-          suround = true,
-          nft = { "rust" },
-          multiline = true,
-        },
         { "{", "}", suround = true, multiline = true },
         { "[", "]", suround = true, multiline = true },
         { "(", ")", suround = true, multiline = true },
