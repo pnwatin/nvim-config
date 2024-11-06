b55158155b0c2a8aa3cc24a6601e6d1ad8663ad7
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2024-11-06 21:28:38 +0100

    chore: go back to noice default cmd line

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -4,18 +4,7 @@ return {
     opts = {
       presets = {
         lsp_doc_border = true,
-        command_palette = true,
-        long_message_to_split = true,
-      },
-      cmdline = {
-        format = {
-          cmdline = false,
-          search_down = false,
-          search_up = false,
-          filter = false,
-          lua = false,
-          help = false,
-        },
+        bottom_search = false,
       },
     },
   },
