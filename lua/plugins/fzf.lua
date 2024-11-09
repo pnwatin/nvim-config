2f62221056beceba976461e59dfa32418da501b8
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2024-11-09 01:57:24 +0100

    chore: set leader space to search buffers instead of files

---
+++ lua/plugins/fzf.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "ibhagwan/fzf-lua",
+    keys = {
+      { "<leader><space>", "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
+      { "<leader>:", false },
+    },
+  },
+}
