4e2a874514d626f5d28b038034005c41c10e1dac
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2024-11-13 17:56:05 +0100

    chore: go back to leader space searching files

--- lua/plugins/fzf.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "ibhagwan/fzf-lua",
-    keys = {
-      { "<leader><space>", "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
-      { "<leader>:", false },
-    },
-  },
-}
