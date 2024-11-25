86237342433412b375ffff5a800fad915a56421b
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2024-11-25 21:31:28 +0100

    fix: don't auto pair "'" in rust files

--- lua/plugins/auto-pair.lua
+++ lua/plugins/auto-pair.lua
@@ -8,6 +8,17 @@ return {
         { "{", "}", suround = true, multiline = true },
         { "[", "]", suround = true, multiline = true },
         { "(", ")", suround = true, multiline = true },
+        {
+          "'",
+          "'",
+          suround = true,
+          cond = function(fn)
+            return not fn.in_lisp() or fn.in_string()
+          end,
+          alpha = true,
+          nft = { "tex", "rust" },
+          multiline = false,
+        },
       },
     },
   },
