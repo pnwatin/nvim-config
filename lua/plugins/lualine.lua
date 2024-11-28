1cd4d632767be18f3dec45b7768efcc1387b2ad7
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2024-11-28 19:21:49 +0100

    chore: go back to lazyvim default for lualine but tweaks appearance and remove time section

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,92 +1,13 @@
----@diagnostic disable: undefined-field
 return {
   {
     "nvim-lualine/lualine.nvim",
-    opts = function()
-      local icons = require("lazyvim.config").icons
-      local Util = require("lazyvim.util")
-
-      return {
-        options = {
-          section_separators = "",
-          component_separators = "",
-          globalstatus = true,
-        },
-        sections = {
-          lualine_a = {
-            {
-              "mode",
-            },
-          },
-          lualine_b = {
-            {
-              "branch",
-            },
-            {
-              "diff",
-              symbols = {
-                added = icons.git.added,
-                modified = icons.git.modified,
-                removed = icons.git.removed,
-              },
-              source = function()
-                local gitsigns = vim.b.gitsigns_status_dict
-                if gitsigns then
-                  return {
-                    added = gitsigns.added,
-                    modified = gitsigns.changed,
-                    removed = gitsigns.removed,
-                  }
-                end
-              end,
-            },
-          },
-          lualine_c = {
-            {
-              "filename",
-              use_mode_colors = true,
-              symbols = {
-                modified = " ●",
-              },
-            },
-            {
-              "diagnostics",
-              symbols = {
-                error = icons.diagnostics.Error,
-                warn = icons.diagnostics.Warn,
-                info = icons.diagnostics.Info,
-                hint = icons.diagnostics.Hint,
-              },
-            },
-          },
-          lualine_x = {},
-          lualine_y = {
-            { "searchcount" },
-            {
-              function()
-                return require("noice").api.status.command.get()
-              end,
-              cond = function()
-                return package.loaded["noice"] and require("noice").api.status.command.has()
-              end,
-              color = Util.ui.fg("Statement"),
-            },
-            {
-              function()
-                return require("noice").api.status.mode.get()
-              end,
-              cond = function()
-                return package.loaded["noice"] and require("noice").api.status.mode.has()
-              end,
-              color = Util.ui.fg("Constant"),
-            },
-          },
-          lualine_z = {
-            { "progress", separator = " ", padding = { left = 1, right = 0 } },
-            { "location", padding = { left = 0, right = 1 } },
-          },
-        },
-      }
-    end,
+    opts = {
+      options = {
+        section_separators = "",
+        component_separators = "",
+        globalstatus = true,
+      },
+      sections = { lualine_z = {} },
+    },
   },
 }
