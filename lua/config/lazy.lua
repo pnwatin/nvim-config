9e98e684f8f2e1325195569addf2a25f84c84046
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2025-03-04 21:51:07 +0100

    chore: add harpoon2 plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -39,6 +39,7 @@ require("lazy").setup({
     -- editor
     { import = "lazyvim.plugins.extras.editor.fzf" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
+    { import = "lazyvim.plugins.extras.editor.harpoon2" },
 
     -- ui
 
