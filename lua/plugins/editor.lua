73f01f0ac00faecba8146b03db8defb78ff15215
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2025-03-29 18:40:32 +0100

    chore: go back to oil.nvim as file explorer

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,7 +1,44 @@
 return {
   {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false,
+    keys = {
+      {
+        "<leader>e",
+        function()
+          local oil = require("oil")
+          oil.toggle_float()
+        end,
+        desc = "Open oil",
+        silent = true,
+      },
+      {
+        "<leader>E",
+        function()
+          local oil = require("oil")
+          local cwd = vim.fn.getcwd()
+          oil.toggle_float(cwd)
+        end,
+        desc = "Open oil (cwd)",
+        silent = true,
+      },
+    },
+    opts = {
+      keymaps = {
+        ["q"] = "actions.close",
+        ["<bs>"] = "actions.parent",
+      },
+      view_options = {
+        show_hidden = true,
+      },
+      skip_confirm_for_simple_edits = true,
+    },
+  },
+  {
     "echasnovski/mini.files",
     lazy = false,
+    enabled = false,
     keys = {
       {
         "<leader>fm",
