70af295a19d79dbe19b0e5cb3f91c00ff043a336
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2025-04-03 15:06:31 +0200

    chore: remove mason/vtsls keys personnalization

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,6 @@
 return {
   {
     "williamboman/mason.nvim",
-    keys = { { "<leader>M", vim.cmd.Mason, desc = "Mason" }, { "<leader>cm", false } },
     opts = {
       ui = { border = "rounded" },
     },
@@ -22,74 +21,6 @@ return {
         virtual_text = false,
       },
       servers = {
-        vtsls = {
-          keys = {
-            {
-              "<leader>cm",
-              LazyVim.lsp.action["source.addMissingImports.ts"],
-              desc = "Add missing imports",
-            },
-            {
-              "<leader>cM",
-              false,
-            },
-            {
-              "gD",
-              function()
-                local params = vim.lsp.util.make_position_params()
-                LazyVim.lsp.execute({
-                  command = "typescript.goToSourceDefinition",
-                  arguments = { params.textDocument.uri, params.position },
-                  open = true,
-                })
-              end,
-              desc = "Goto Source Definition",
-            },
-            {
-              "gR",
-              function()
-                LazyVim.lsp.execute({
-                  command = "typescript.findAllFileReferences",
-                  arguments = { vim.uri_from_bufnr(0) },
-                  open = true,
-                })
-              end,
-              desc = "File References",
-            },
-            {
-              "<leader>co",
-              LazyVim.lsp.action["source.organizeImports"],
-              desc = "Organize Imports",
-            },
-            {
-              "<leader>cu",
-              LazyVim.lsp.action["source.removeUnusedImports"],
-              desc = "Remove unused imports",
-            },
-            {
-              "<leader>cU",
-              LazyVim.lsp.action["source.removeUnused.ts"],
-              desc = "Remove unused",
-            },
-            {
-              "<leader>cD",
-              LazyVim.lsp.action["source.fixAll.ts"],
-              desc = "Fix all diagnostics",
-            },
-            {
-              "<leader>cV",
-              function()
-                LazyVim.lsp.execute({ command = "typescript.selectTypeScriptVersion" })
-              end,
-              desc = "Select TS workspace version",
-            },
-            {
-              "<leader>ce",
-              "<Cmd>EslintFixAll<CR>",
-              desc = "Fix all eslint diagnostics",
-            },
-          },
-        },
         tailwindcss = {
           init_options = {
             userLanguages = {
