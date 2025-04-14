de947bac0ab7c435c2a8de5a3bf8708d7ce3b0ad
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2025-04-14 23:45:40 +0200

    chore: enable noice lsp signature

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -22,7 +22,7 @@ return {
       messages = {
         enabled = false,
       },
-      lsp = { signature = { enabled = false } },
+      lsp = { signature = { enabled = true } },
     },
   },
   {
