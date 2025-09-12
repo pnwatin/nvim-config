79dd2c4cbb5170fc7877769d0e68b359d2b7eedb
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2025-09-12 18:02:57 +0200

    chore: filter out no information available notifications

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -10,6 +10,15 @@ return {
   {
     "folke/noice.nvim",
     opts = {
+      routes = {
+        {
+          filter = {
+            event = "notify",
+            find = "No information available",
+          },
+          opts = { skip = true },
+        },
+      },
       presets = {
         lsp_doc_border = true,
         bottom_search = true,
