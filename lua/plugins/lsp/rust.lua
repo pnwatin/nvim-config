fb7486b2c60b0f36b280c33d5d36fbb42b07eaa7
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2025-12-28 23:10:28 +0100

    chore: rustanceanvim dont ignore procMacros like async_trait

--- lua/plugins/lsp/rust.lua
+++ lua/plugins/lsp/rust.lua
@@ -19,11 +19,6 @@ return {
             },
             procMacro = {
               enable = true,
-              ignored = {
-                ["async-trait"] = { "async_trait" },
-                ["napi-derive"] = { "napi" },
-                ["async-recursion"] = { "async_recursion" },
-              },
             },
             files = {
               excludeDirs = {
