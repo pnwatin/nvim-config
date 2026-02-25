a6554cf8c018444938312e6df5380de5e1af6520
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2026-02-25 22:50:36 +0100

    feat: make sure that lazygit doesn't open a new window

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -19,7 +19,17 @@ return {
         animate = { enabled = false },
         scope = { char = "╎", hl = "ModeMsg" },
       },
-      lazygit = { enabled = true },
+      lazygit = {
+        enabled = true,
+        config = {
+          os = {
+            editPreset = "nvim-remote",
+            edit = '[ -z "$NVIM" ] && (nvim -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{filename}})',
+            editAtLine = '[ -z "$NVIM" ] && (nvim +{{line}} -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{filename}} && nvim --server "$NVIM" --remote-send ":{{line}}<CR>")',
+            openDirInEditor = '[ -z "$NVIM" ] && (nvim -- {{dir}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{dir}})',
+          },
+        },
+      },
       rename = {
         enabled = true,
       },
