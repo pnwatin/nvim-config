3f3c8cca0a803c230c320e3262a916ced771cf45
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2026-08-29 19:26:40 +0200

    chore: enable line wrap by default

--- init.lua
+++ init.lua
@@ -47,7 +47,7 @@ vim.opt.virtualedit = "block" -- Allow cursor to move where there is no text in 
 vim.opt.wildmode = "longest:full,full" -- Command-line completion mode
 vim.opt.winborder = "rounded"
 vim.opt.winminwidth = 5 -- Minimum window width
-vim.opt.wrap = false -- Disable line wrap
+vim.opt.wrap = true -- Enable line wrap by default
 vim.opt.fillchars:append({ msgsep = "-" })
 
 -- disable netrw completely
